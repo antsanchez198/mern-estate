b7b933fd23f3a8c116b9eb89affc52f4fba0dcf4
Anthony Sanchez <antsanchez198@gmail.com>
2024-01-21 23:28:35 -0500

    User API testing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -12,12 +13,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Testing server! :)');
 })
 
-app.get('/test', (req, res) => {
-    res.send("Hello World")
-})
-
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
